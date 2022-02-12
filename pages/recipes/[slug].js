85490b0710d2c39c4e89e0d3938203d925047342
harry <harry-joevalente@live.co.uk>
2022-02-12 11:00:37 +0000

    initial

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -89,7 +89,7 @@ export default function RecipeDetails({recipe}) {
         .info span:last-child::after {
           content: ".";
         }
-      `}</style>
+      `}</style>    
     </div>
   )
 }
